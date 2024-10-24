46df784d52e4778cc4fa0053d10c00dd6c382be6
MikeCynic <michaelshmidtt@gmail.com>
2024-10-24 07:05:57 +0300

    Update task1.py

--- coding_lab1/task1.py
+++ coding_lab1/task1.py
@@ -1,12 +1,12 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-s = 0
+numbers_sum = 0
 for n in numbers:
     try:
-        s += n
+        numbers_sum += n
     except TypeError:
         continue
-average = s / len(numbers)
+average = numbers_sum / len(numbers)
 numbers[numbers.index(None)] = average
 print("Измененный список:", numbers)
