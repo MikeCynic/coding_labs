c49c0545319cc2dbad54b5e8a3c11a6fc51e9603
MikeCynic <michaelshmidtt@gmail.com>
2024-11-09 17:58:36 +0300

    Update task2.py

--- coding_lab3/task2.py
+++ coding_lab3/task2.py
@@ -4,17 +4,15 @@
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
+
 # TODO Провеьте работу функции с разделителем отличным от запятой
 
 
-def find_common_participants(p1, p2, d=','):
-    p_list = []
-    p1 = p1.split(d)
-    p2 = p2.split(d)
-    for p in p1:
-        if p in p2:
-            p_list.append(p)
-    p_list=sorted(p_list)
-    return p_list
-
-
+def find_common_participants(participant_group_1, participant_group_2, divider=','):
+    common_participant_list = []
+    participant_group_1 = participant_group_1.split(divider)
+    participant_group_2 = participant_group_2.split(divider)
+    for participant in participant_group_1:
+        if participant in participant_group_2:
+            common_participant_list.append(participant)
+    return sorted(common_participant_list)
